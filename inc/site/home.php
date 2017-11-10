c4da5502f1a414f3b64a06e921e1758fb334884e
hummerk10 <hummerk10@gmail.com>
2017-11-10 21:34:26 +0000

    Updated Home Page

--- inc/site/home.php
+++ inc/site/home.php
@@ -1,4 +1,31 @@
 <?php
 
 ?>
-WELCOME TO THE HOME PAGE!!!!
+<div class="row text-center">
+<div class="col-sm-12">Brew Time!!!</div>
+</div>
+
+
+<div class="row text-center">
+  <div class="col-sm-4">
+  <h2>HTL</h2>
+  <h3>Temp:</h3>
+    <img src="inc/img/Vessel.jpg." alt="HLT">
+  </div>
+  <div class="col-sm-4">
+  <h2>Mash</h2> 
+  <h3>Temp:</h3> 
+  <img src="inc/img/Vessel.jpg." alt="Mash">
+  </div>
+  <div class="col-sm-4">
+  <h2>Boil</h2>
+  <h3>Temp:</h3>
+  <img src="inc/img/Vessel.jpg." alt=Boil">
+  </div>
+</div>
+
+<div class="row text-center">
+  <div class="col-sm-6"><h3>Pump 1</h3></div>
+  <div class="col-sm-6"><h3>Pump 2 (NOT REAL) </h3></div>
+  
+</div>
