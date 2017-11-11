b18947cbb498950bb48cb226a1b10e3bf5031630
hummerk10 <hummerk10@gmail.com>
2017-11-11 12:09:02 +0000

    Home screen formatting

--- inc/site/home.php
+++ inc/site/home.php
@@ -17,31 +17,43 @@ foreach ($r as $num => $row) {
 }
 ?>
 
-<div class="row text-center">
-<div class="col-sm-12">Brew Time!!!</div>
-</div>
-
 
 <div class="row text-center">
   <div class="col-sm-4">
   <h2>HTL</h2>
   <h3>Temp:<?php echo $hlt ?></h3>
     <img src="inc/img/Vessel.jpg." alt="HLT">
-  </div>
-  <div class="col-sm-4">
+</div>
+ <div class="col-sm-4">
   <h2>Mash</h2> 
   <h3>Temp:<?php echo $mash ?></h3> 
   <img src="inc/img/Vessel.jpg." alt="Mash">
-  </div>
-  <div class="col-sm-4">
+ </div>
+ <div class="col-sm-4">
   <h2>Boil</h2>
   <h3>Temp:<?php echo $boil ?></h3>
   <img src="inc/img/Vessel.jpg." alt=Boil">
-  </div>
+ </div>
 </div>
 
 <div class="row text-center">
-  <div class="col-sm-6"><h3>Pump 1</h3></div>
-  <div class="col-sm-6"><h3>Pump 2 (NOT REAL) </h3></div>
+  <div class="col-sm-6">
+  <h2>Flow 1</h2>
+  <h3> Gallons: 00 </h3>
+  </div>
+  <div class="col-sm-6">
+  <h2>Flow 2 </h2>
+  <h3> Gallons: 00 </h3>
+  </div>
+
+<div class="row text-center">
+  <div class="col-sm-6">
+  <h2>Pump 1</h2>
+  <h3> Status: OFF </h3>
+  </div>
+  <div class="col-sm-6">
+  <h2>Pump 2 (NOT REAL) </h2>
+  <h3> Status: OFF </h3>
+  </div>
   
 </div>
